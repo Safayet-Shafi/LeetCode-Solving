d5c166684698623b6c5c0fe76a6df53dac665fde
Safayet-Shafi <77004144+Safayet-Shafi@users.noreply.github.com>
2024-01-02 00:09:48 +0600

    1480 solve

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        SmallestPositiveMultiple2413 obj1=new SmallestPositiveMultiple2413();
-        obj1.smallestEvenMultiple(5);
+//        2415
+          SmallestPositiveMultiple2413 obj1=new SmallestPositiveMultiple2413();
+//        obj1.smallestEvenMultiple(5);
+
+
+        int[]result1480=obj1.runningSum(new int[]{1,2,3,4});
+        for (int num : result1480) {
+            System.out.print(num + " ");
+        }
     }
 }
--- src/SmallestPositiveMultiple2413.java
+++ src/SmallestPositiveMultiple2413.java
@@ -6,4 +6,16 @@ public class SmallestPositiveMultiple2413 {
         else return n*2;
 
     }
+
+
+    public int[] runningSum(int[] nums) {
+        int [] temp = new int[nums.length];
+        int sum =0;
+        for(int i=0;i<nums.length;i++){
+            sum =sum+nums[i];
+            temp[i]=sum;
+
+        }
+        return  temp;
+    }
 }
