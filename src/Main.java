29514a20411aab5fa4b001dcca0b092d096320db
Safayet-Shafi <77004144+Safayet-Shafi@users.noreply.github.com>
2024-04-10 03:16:39 +0600

    fd

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,22 @@ public class Main {
           SmallestPositiveMultiple2413 obj1=new SmallestPositiveMultiple2413();
 //        obj1.smallestEvenMultiple(5);
 
-
+        //1480
         int[]result1480=obj1.runningSum(new int[]{1,2,3,4});
-        for (int num : result1480) {
-            System.out.print(num + " ");
-        }
+//        for (int num : result1480) {
+//            System.out.print(num + " ");
+//        }
+        // max96
+//        System.out.println(obj1.maximum69Number(9669));
+
+//
+        boolean result = obj1.isPangram("thequickbrownfoxjumpsoverthelazydog");
+//        System.out.println(result);
+
+        int number=obj1.numberOfEmployeesWhoMetTarget(new int[]{5,1,4,2,2},6);
+//        System.out.println(number);
+
+        String result1 = obj1.restoreString("codeleet",new int []{4,5,6,7,0,2,1,3});
+        System.out.println(result1);
     }
 }
--- src/SmallestPositiveMultiple2413.java
+++ src/SmallestPositiveMultiple2413.java
@@ -1,21 +1,84 @@
 public class SmallestPositiveMultiple2413 {
+
     public int smallestEvenMultiple(int n) {
-        if(n%2==0){
+        if (n % 2 == 0) {
             return n;
-        }
-        else return n*2;
+        } else return n * 2;
 
     }
-
 
     public int[] runningSum(int[] nums) {
-        int [] temp = new int[nums.length];
-        int sum =0;
-        for(int i=0;i<nums.length;i++){
-            sum =sum+nums[i];
-            temp[i]=sum;
+        int[] temp = new int[nums.length];
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum = sum + nums[i];
+            temp[i] = sum;
 
         }
-        return  temp;
+        return temp;
+    }
+
+    int maximum69Number(int num) {
+        String s = Integer.toString(num);
+        char[] ch = s.toCharArray();
+
+        for (int i = 0; i < ch.length; i++) {
+            if (ch[i] == '6') {
+                ch[i] = '9';
+                break;
+            }
+
+        }
+        return Integer.parseInt(new String(ch));
+    }
+
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        String s1 = String.join("", word1);
+        String s2 = String.join("", word2);
+
+        if (s1.equals(s2)) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+
+
+    public boolean isPangram(String sentance) {
+        int Alphabet[] = new int[26];
+        for (char ch : sentance.toCharArray()) {
+            if (Character.isAlphabetic(ch)) {
+                Alphabet[ch - 'a'] = Alphabet[ch - 'a'] + 1;
+            }
+        }
+        for (int count : Alphabet) {
+            if (count == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public int numberOfEmployeesWhoMetTarget(int[] hours, int target) {
+        int count =0;
+        for(int i=0;i<hours.length;i++){
+            if(hours[i]>=target){
+                count ++;
+
+            }
+        }
+        return  count;
+    }
+
+
+    public String restoreString(String s, int[] indices) {
+      char [] suffeled = new char [s.length()];
+      for(int i=0;i<indices.length;i++){
+          suffeled[indices[i]]=s.charAt(i);
+      }
+      return new String(suffeled);
     }
 }
+
